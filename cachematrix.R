1492c755cdc5e2aac97b374e610c75ef5d17e8ad
Rick Camp <rjc3@outlook.com>
2015-07-25 15:10:22 -0700

    Functions work, comments still needed. Testing commit.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+        matrx <- NULL
+        set <- function(y) {
+                x <<- y
+                matrx <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) matrx <<- solve
+        getinv <- function() matrx
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        matrx <- x$getinv()
+        if(!is.null(matrx)) {
+                message("getting cached data")
+                return(matrx)
+        }
+        data <- x$get()
+        matrx <- solve(data, ...)
+        x$setinv(matrx)
+        matrx
         ## Return a matrix that is the inverse of 'x'
 }
